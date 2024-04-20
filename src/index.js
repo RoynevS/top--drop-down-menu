29c1625a2d3dd106685ebc8ebe95926304679cea
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-04-20 18:59:07 +0200

    Create drop down navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles/style.css";
+
+const openDropDown = (function () {
+  const hoverItem = document.querySelector(".hover-item");
+  const dropDown = document.querySelector(".drop-down");
+  const nav = document.querySelector("nav");
+
+  const hoverHandler = (event) => {
+    event.type === "mouseover"
+      ? (dropDown.style.visibility = "visible")
+      : (dropDown.style.visibility = "hidden");
+  };
+
+  hoverItem.addEventListener("mouseover", hoverHandler);
+  nav.addEventListener("mouseleave", hoverHandler);
+})();
